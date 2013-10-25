0b34ce8f7122bc76cedbdee8b4f0b5da270e59fc
lukacsg <lukacsg@ilab.sztaki.hu>
2013-10-25 11:25:18 +0200

    Some Stub add to the project.

--- src/main/java/hu/sztaki/ilab/cumulonimbus/als2/SumReduce.java
+++ src/main/java/hu/sztaki/ilab/cumulonimbus/als2/SumReduce.java
@@ -10,8 +10,11 @@ import eu.stratosphere.nephele.configuration.Configuration;
 import eu.stratosphere.pact.common.stubs.Collector;
 import eu.stratosphere.pact.common.stubs.ReduceStub;
 import eu.stratosphere.pact.common.type.PactRecord;
+import eu.stratosphere.pact.common.type.base.PactDouble;
 import eu.stratosphere.pact.common.type.base.PactInteger;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  *
@@ -21,22 +24,32 @@ public class SumReduce extends ReduceStub {
     private final PactRecord output = new PactRecord();
     private double sum;
     private PactRecord tmp;
-
-    private int k;
+    private List<Double> list = new ArrayList<Double>();
+//    private int k;
     private int index;
-    private final PactRecord output_ = new PactRecord();
 
     @Override
     public void open(Configuration parameters) throws Exception {
-        k = parameters.getInteger(ALS.K, 1);
+//        k = parameters.getInteger(ALS.K, 1);
         index = parameters.getInteger(ALS.INDEX, 1);
     }
 
     @Override
     public void reduce(Iterator<PactRecord> records, Collector<PactRecord> out) throws Exception {
         tmp = records.next();
-        output.setField(0, tmp.getField(0, PactInteger.class));
-        sum = tmp.getField(3, null)
+        output.setField(0, tmp.getField(index, PactInteger.class));
+        for(int i= 2;i < tmp.getNumFields(); ++i) {
+            list.add(i-2, tmp.getField(i, PactDouble.class).getValue());
+        }
+        while(records.hasNext()) {
+            tmp = records.next();
+            for (int i = 0; i < list.size(); ++i) {
+                list.set(i, list.get(i) + tmp.getField(i + 2, PactDouble.class).getValue());
+            }
+        }
+        for (int i = 0; i < list.size(); ++i) {
+            output.setField(i+1, new PactDouble(list.get(i)));
+        }
     }
     
 }
