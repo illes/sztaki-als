f37064399df47e5b26bf5eba6b87f7235b916239
Illes Solt <illes@gravityrd.com>
2013-10-25 14:12:58 +0200

    fix for multiple threads

--- eval.py
+++ eval.py
@@ -4,6 +4,9 @@
 import numpy as np
 import scipy.sparse as sp
 import sys
+import os
+from numpy.core.numeric import ndarray
+import glob
 
 
 def loadsparse(fname):
@@ -29,11 +32,24 @@ def loaddense(fname):
     Load dense matrix from CSV file, with index as first column
     """
     print "Loading %s ..." % (fname)
-    #return np.asmatrix(np.loadtxt(open(fname,"rb"),delimiter=" ",skiprows=0))
-    nda = np.loadtxt(open(fname,"rb"),delimiter=" ",skiprows=0)
+    if os.path.isdir(fname):
+        ndas = []
+        for f in glob.glob(os.path.join(fname, '*')):
+            #return np.asmatrix(np.loadtxt(open(fname,"rb"),delimiter=" ",skiprows=0))
+            ndas.append(np.loadtxt(open(f,"rb"),delimiter=" ",skiprows=0))
+        nda = np.vstack(ndas)
+
+    else:
+        #return np.asmatrix(np.loadtxt(open(fname,"rb"),delimiter=" ",skiprows=0))
+        nda = np.loadtxt(open(fname,"rb"),delimiter=" ",skiprows=0)
+
+    # print (str(nda))
     ids = nda[:,0].astype(int)
+    ids_inv = np.empty(ids.shape, int)
+    for idx, id in enumerate(ids):
+        ids_inv[id] = idx
     data = nda[:,1:]
-    return data[ids]
+    return data[ids_inv]
     
 def objfunc(R, P, Q, lambd):
     sse = (((P[R.row] * Q[R.col]).sum(1) - R.data)**2).sum()
