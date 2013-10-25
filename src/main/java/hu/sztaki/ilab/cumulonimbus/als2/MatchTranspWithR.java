8ae5e92ab9ee49fc10a1d799de013d1de41b2bbf
Lorand Kasler <kasler.lorand@virgo.hu>
2013-10-25 12:31:28 +0200

    multiwith transp

--- src/main/java/hu/sztaki/ilab/cumulonimbus/als2/MatchTranspWithR.java
+++ src/main/java/hu/sztaki/ilab/cumulonimbus/als2/MatchTranspWithR.java
@@ -1,17 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package hu.sztaki.ilab.cumulonimbus.als2;
 
-//import eu.stratosphere.nephele.configuration.Configuration;
-import eu.stratosphere.nephele.configuration.Configuration;
+
 import eu.stratosphere.pact.common.stubs.Collector;
 import eu.stratosphere.pact.common.stubs.MatchStub;
 import eu.stratosphere.pact.common.type.PactRecord;
-import eu.stratosphere.pact.common.type.base.PactDouble;
 import eu.stratosphere.pact.common.type.base.PactInteger;
 
 /**
@@ -23,12 +17,11 @@ public class MatchTranspWithR extends MatchStub {
 	private final PactRecord output = new PactRecord();
 
 
-
 	@Override
 	public void match(PactRecord mat, PactRecord r, Collector<PactRecord> out) throws Exception {
 		output.setField(0, r.getField(0, PactInteger.class));
 		output.setField(1, r.getField(1, PactInteger.class));
-		MultWithTransp.setFields(output, 2, MultWithTransp.readFields(mat,1, mat.getNumFields()-1));
+		MultWithTransp.setFields(output, 2, MultWithTransp.readFields(mat, 1, mat.getNumFields() - 1));
 		out.collect(output);
 	}
 
--- src/main/java/hu/sztaki/ilab/cumulonimbus/als2/SumMat.java
+++ src/main/java/hu/sztaki/ilab/cumulonimbus/als2/SumMat.java
@@ -1,37 +1,52 @@
 package hu.sztaki.ilab.cumulonimbus.als2;
 
-import eu.stratosphere.nephele.configuration.Configuration;
-import eu.stratosphere.pact.common.stubs.CoGroupStub;
-import eu.stratosphere.pact.common.stubs.Collector;
-import eu.stratosphere.pact.common.stubs.MatchStub;
-import eu.stratosphere.pact.common.type.PactRecord;
-import eu.stratosphere.pact.common.type.base.PactInteger;
-
 import java.util.Iterator;
+
+import eu.stratosphere.nephele.configuration.Configuration;
+import eu.stratosphere.pact.common.stubs.Collector;
+import eu.stratosphere.pact.common.stubs.ReduceStub;
+import eu.stratosphere.pact.common.type.PactRecord;
+import eu.stratosphere.pact.common.type.base.PactDouble;
+import eu.stratosphere.pact.common.type.base.PactInteger;
 
 
 /**
- * match (j, qj*transp(qij)) (i,j,rij) on val1.j = val2.getField(1-index)
+ * reduce (i, j, qj*transp(qij)) on 1-index
  * emits (val2.getField(index), sumMat)
  *
  * @author klorand
  */
-public class SumMat extends CoGroupStub {
+public class SumMat extends ReduceStub {
 
 	// initialize reusable mutable objects
 	private final PactRecord outputRecord = new PactRecord();
-
-    private int index;
+	private int index;
 
 
 	@Override
 	public void open(Configuration parameters) throws Exception {
 		super.open(parameters);
-        index = parameters.getInteger(ALS.INDEX, 1);
+		index = parameters.getInteger(ALS.INDEX, 1);
 	}
 
-    @Override
-    public void coGroup(Iterator<PactRecord> records1, Iterator<PactRecord> records2, Collector<PactRecord> out) throws Exception {
 
+	@Override
+    public void reduce(Iterator<PactRecord> records, Collector<PactRecord> out) throws Exception {
+       PactInteger emmittedKey = null;
+       double[ ] summed = null;
+       while (records.hasNext()) {
+           PactRecord rec = records.next();
+           if (emmittedKey==null) {
+               emmittedKey = rec.getField(index, PactInteger.class);
+           }
+           if (summed==null) {
+               summed = new double[rec.getNumFields()-2];
+           }
+           for (int i=0; i<summed.length; ++i) {
+               summed[i] += rec.getField(2+i, PactDouble.class).getValue();
+           }
+       }
+       outputRecord.setField(0, emmittedKey);
+       MultWithTransp.setFields(outputRecord, 1, summed);
     }
 }
