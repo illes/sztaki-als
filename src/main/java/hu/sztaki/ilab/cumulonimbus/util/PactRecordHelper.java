4e648865212d8667c1492cd673cc889ddc50c86d
Lorand Kasler <kasler.lorand@virgo.hu>
2013-10-25 16:09:29 +0200

    mult transp

--- src/main/java/hu/sztaki/ilab/cumulonimbus/util/PactRecordHelper.java
+++ src/main/java/hu/sztaki/ilab/cumulonimbus/util/PactRecordHelper.java
@@ -11,7 +11,7 @@ public class PactRecordHelper {
         double[] ret = new double[a.length * b.length];
         for (int i = 0; i < a.length; ++i) {
             for (int j = 0; j < b.length; ++j) {
-                ret[i * (a.length-1) + j] = a[i] * b[j];
+                ret[i * b.length + j] = a[i] * b[j];
             }
         }
 
--- src/test/java/hu/sztaki/ilab/cumulonimbus/als2/MultWithTranspTest.java
+++ src/test/java/hu/sztaki/ilab/cumulonimbus/als2/MultWithTranspTest.java
@@ -14,6 +14,8 @@ public class MultWithTranspTest {
     public void testTransp() {
        double[] ret = PactRecordHelper.transpmultiply(new double[]{1, 2, 3}, new double[]{1, 2});
        assertThat(ret, is(new double[]{1, 2, 2, 4, 3, 6}));
+        double[] ret2 = PactRecordHelper.transpmultiply(new double[]{1, 2, 3}, new double[]{1, 2, 3});
+        assertThat(ret2, is(new double[]{1, 2, 3, 2, 4, 6, 3, 6, 9}));
     }
 
 
